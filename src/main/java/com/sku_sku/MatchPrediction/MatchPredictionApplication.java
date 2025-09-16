29bef54352605c80ed1aeb3eeb88ba75f4727aa3
GOH01 <wave4446@naver.com>
2025-09-16 20:03:55 +0900

    first commit

---
+++ src/main/java/com/sku_sku/MatchPrediction/MatchPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.sku_sku.MatchPrediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchPredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sku_sku/MatchPrediction/MatchPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sku_sku.MatchPrediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

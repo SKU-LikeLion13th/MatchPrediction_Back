4a7af91a9e7e0f7a5e4c7b20d552fe7e4040d710
GOH01 <wave4446@naver.com>
2025-09-20 19:10:09 +0900

    [fix] MatchPredictionApplication

--- src/main/java/com/sku_sku/MatchPrediction/MatchPredictionApplication.java
+++ src/main/java/com/sku_sku/MatchPrediction/MatchPredictionApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class MatchPredictionApplication {
 
 	public static void main(String[] args) {
